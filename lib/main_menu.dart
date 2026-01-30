ff1cbeb52b906e49e8304086d8d1dbd6ff405961
Michael Fernando <michaelfernando784@gmail.com>
2026-01-30 18:47:56 +0700

    fix : - Tambahan package flame dan flutter_screenutil - Tambahan halaman main menu

---
+++ lib/main_menu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatefulWidget {
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
